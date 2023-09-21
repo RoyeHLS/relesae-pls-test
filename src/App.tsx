294e6d38c08e2b9c9cd732ca771b9b63fb765c4a
roye <roye@moveohls.com>
2023-09-21 11:22:30 +0300

    feat: giant feature i am the king

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <p>created a giant feature</p>
+    </div>
+  );
 };
 
 export default App;
